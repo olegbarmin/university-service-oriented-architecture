624d15eea47391749595d076dec993cd39fbbf55
olegbarmin <olegbarmin@gmail.com>
2020-05-26 23:32:57 +0300

    Initial Commit

---
+++ StudentsService/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Students.Models
+{
+    [Serializable]
+    public class Student
+    {
+        public string Name { set; get; }
+
+        public string Surname { set; get; }
+
+        public float AvgMark { set;  get; }
+    }
+}
---
+++ StudentsService/StudentsService.asmx.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using System.Web.Services;
+using System.Web.Hosting;
+using System.Xml.Serialization;
+using System.IO;
+using Students.Models;
+
+namespace Students
+{
+
+    [WebService(Name = "StudentsService", Namespace = "http://www.friends.com/")]
+    public class StudentsService : WebService
+    {
+        const string PATH_TO_STUDENTS_XML_FILE = "~/friends.xml";
+
+        [WebMethod]
+        public Student[] GetAllStudents()
+        {
+            return LoadStudentsFromFile();
+        }
+
+        [WebMethod]
+        public Student[] GetStudentsFilteredByAverageMark(float lowerBound, float upperBound)
+        {
+            var students = LoadStudentsFromFile();
+            return students.Where(s => s.AvgMark >= lowerBound && s.AvgMark <= upperBound).ToArray();
+        }
+
+        private Student[] LoadStudentsFromFile()
+        {
+            var pathToFile = HostingEnvironment.MapPath(PATH_TO_STUDENTS_XML_FILE);
+            var formatter = new XmlSerializer(typeof(Student[]));
+            Student[] students;
+            using (FileStream fs = new FileStream(pathToFile, FileMode.OpenOrCreate))
+            {
+                students = (Student[])formatter.Deserialize(fs);
+            }
+            return students.ToArray();
+        }
+    }
+}

52d19d102da076bdd6e3a3431afe4ee0e5569d75
olegbarmin <olegbarmin@gmail.com>
2020-05-28 23:04:32 +0300

    Add solution for the task 4

---
+++ Task4/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Task4.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly StudentsService.StudentsServiceSoapClient client = new StudentsService.StudentsServiceSoapClient();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult getAllStudents()
+        {
+            var studentsArr = client.GetAllStudents();
+            return View("Result", studentsArr);
+        }
+
+        [HttpPost]
+        public ActionResult GetStudentsFilteredByAverageMark(string lowerBound, string upperBound)
+        {
+            var lowerBoundValue = Convert.ToInt32(lowerBound);
+            var upperBoundValue = Convert.ToInt32(upperBound);
+
+            var studentsArr = client.GetStudentsFilteredByAverageMark(lowerBoundValue, upperBoundValue);
+            return View("Result", studentsArr);
+        }
+    }
+}

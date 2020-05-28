c37d46be2603ab90824e6daef1214a0e83931cf4
olegbarmin <olegbarmin@gmail.com>
2020-05-29 00:41:16 +0300

    Add solution for the task 5

---
+++ StudentWPF/Helpers/RelayCommand.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WpfAppForTask5.Helpers
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action execute;
+
+        private readonly Func<bool> canExecute;
+        private ICommand videoIsCheked;
+
+        public event EventHandler CanExecuteChanged;
+
+        public RelayCommand(Action execute)
+            : this(execute, null)
+        {
+        }
+
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            this.canExecute = canExecute;
+        }
+
+        public RelayCommand(ICommand videoIsCheked)
+        {
+            this.videoIsCheked = videoIsCheked;
+        }
+
+        public bool CanExecute(object parameter) => canExecute == null || canExecute();
+
+        public void Execute(object parameter) => execute();
+
+        public void OnCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> execute;
+
+        private readonly Func<T, bool> canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public RelayCommand(Action<T> execute)
+            : this(execute, null)
+        {
+        }
+
+        public RelayCommand(Action<T> execute, Func<T, bool> canExecute)
+        {
+            this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter) => canExecute == null || canExecute((T)parameter);
+
+        public void Execute(object parameter) => execute((T)parameter);
+
+        public void OnCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
---
+++ StudentWPF/MainWindowViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using WpfAppForTask5.Helpers;
+using WpfAppForTask5.StudentsService;
+
+namespace WpfAppForTask5
+{
+    public class MainWindowViewModel : Observable
+    {
+        private readonly StudentsService.StudentsService service = new StudentsService.StudentsService();
+
+        private ICommand showAllStudentsCmd;
+        private ICommand showFilteredStudentsCmd;
+
+        private string getStudentsInRangeMin = "1";
+        private string getStudentsInRangeMax = "5";
+
+        public ObservableCollection<Student> StudentsTable { get; } = new ObservableCollection<Student>();
+    
+        public string lowerBoundInput
+        {
+            get { return getStudentsInRangeMin; }
+            set { Set(ref getStudentsInRangeMin, value); }
+        }
+        public string upperBoundInput
+        {
+            get { return getStudentsInRangeMax; }
+            set { Set(ref getStudentsInRangeMax, value); }
+        }
+
+        public ICommand showAllStudentClick => showAllStudentsCmd ?? (showAllStudentsCmd = new RelayCommand(LowerSubmitClick));
+
+        public ICommand filteredStudentsClick => showFilteredStudentsCmd ?? (showFilteredStudentsCmd = new RelayCommand(InRangeSubmitClick));
+
+        private void LowerSubmitClick()
+        {
+            var temp = service.GetAllStudents();
+            populateTable(temp);
+        }
+
+        private void InRangeSubmitClick()
+        {
+            float lowerBound;
+            float upperBound;
+            if (float.TryParse(lowerBoundInput, out lowerBound) && float.TryParse(upperBoundInput, out upperBound))
+            {
+                var studentsArr = service.GetStudentsFilteredByAverageMark(lowerBound, upperBound);
+                populateTable(studentsArr);
+            }
+        }
+
+        private void populateTable(Student[] students)
+        {
+            StudentsTable.Clear();
+            Array.ForEach(students, s => StudentsTable.Add(s));
+        }
+    }
+}
